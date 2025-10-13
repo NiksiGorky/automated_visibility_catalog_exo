de0f056c9999b7acb65a96160d8b8f415b54a51c
NiksiGorky <154052873+NiksiGorky@users.noreply.github.com>
2025-10-13 19:06:02 +0530

    Add files via upload

---
+++ Automated_Visibility_Catalog.py
@@ -0,0 +1,440 @@
+import streamlit as st
+import base64
+
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz, get_sun
+import astropy.units as u
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.time import Time
+
+import pandas as pd
+
+from timezonefinder import TimezoneFinder
+import datetime
+from zoneinfo import ZoneInfo
+
+st.set_page_config(page_title="Automated Visibility Catalog", layout="wide")
+
+st.markdown(
+    "<h1 style='text-align: center;'>🔭 Automated Visibility Catalog For Exoplanet Host Stars</h1>",
+    unsafe_allow_html=True
+)
+
+st.markdown("<br>", unsafe_allow_html=True) 
+
+st.markdown("""
+<style>
+    /* Style of the text input */
+    div[data-testid="stTextInput"] label p {
+        font-size: 16px !important;
+    }
+    .stTextInput {
+        width: 275px !important;
+    }
+            
+    /* Style of the date input */
+    div[data-testid="stDateInput"] label p {
+        font-size: 16px !important;
+    }
+    .stDateInput {
+        width: 275px !important;
+    }
+            
+    /* Style for file uploader */
+    div[data-testid="stFileUploader"] {
+        width: 300px !important;
+    }
+    div[data-testid="stFileUploader"] section {
+        width: 425px !important;
+    }
+    
+    /* Change file uploader label text size */
+    div[data-testid="stFileUploader"] label p {
+        font-size: 16px !important;
+    }
+            
+</style>
+
+""", unsafe_allow_html=True)
+
+for key in ["Data", "has_valid_magnitude", "utc_shift", "Loc", "timezone_local", "Star_Observability"]:
+    st.session_state.setdefault(key, None)
+
+def is_numeric(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+    
+#Function to read data from a file
+
+@st.cache_data
+def read_data(file_path):
+
+    has_valid_magnitude = False
+
+    try:
+        file=pd.read_csv(file_path, sep=None, engine='python')
+
+    except Exception as e:
+        raise ValueError('Error Loading File')
+    
+    file.columns = [c.strip().lower() for c in file.columns]
+
+    name_col = next((c for c in file.columns if 'name' in c), None)
+    mag_col = next((c for c in file.columns if 'mag' in c), None)
+    ra_col = next((c for c in file.columns if 'ra' in c), None)
+    dec_col = next((c for c in file.columns if 'dec' in c), None)
+    coord_col = next((c for c in file.columns if 'coord' in c), None)
+
+    if ra_col and dec_col:
+        ra = file[ra_col]
+        dec = file[dec_col]
+    elif coord_col:
+        ra = file[coord_col].str.split(',').str[0]
+        dec = file[coord_col].str.split(',').str[1]
+
+    else:
+        raise ValueError('No RA/DEC data found in the file. Use valid data/coloumn name')
+    
+    if not name_col is None:
+        star_name=file[name_col]
+
+    else:
+        raise ValueError('Name of the stars not found')
+    
+    
+    if not (pd.to_numeric(ra, errors="coerce").notna().all() and 
+            pd.to_numeric(dec, errors="coerce").notna().all()
+            ):
+        raise ValueError("RA or DEC contains invalid values.")
+
+    if file[[ra_col, dec_col, name_col]].isnull().any().any():
+        raise ValueError("One or more required columns contain none values.")
+    
+    if mag_col:
+        mag = file[mag_col]
+        
+        # Check if magnitude values are valid
+        if (pd.to_numeric(mag, errors="coerce").notna().all() and
+            not file[[mag_col]].isnull().any().any()):
+            has_valid_magnitude = True
+        else:
+            # Show warning but still include the column
+            st.warning("Warning: MAG column contains invalid values.", width=290)
+        
+        # Always create DataFrame with mag column if it exists
+        Data = pd.DataFrame({
+            'star name': star_name,
+            'ra': ra,
+            'dec': dec,
+            'mag': mag
+        })
+    else:
+        Data = pd.DataFrame({
+            'star name': star_name,
+            'ra': ra,
+            'dec': dec
+        })
+        
+    del file
+    return Data, has_valid_magnitude
+
+@st.cache_data
+def Observability(Data, _utc_shift,date, _Loc, timezone_local):
+
+    date_str = date.strftime("%Y-%m-%d")
+
+    #12 noon in local time zone
+    time_in_local = Time( f"{date} 12:00:00")-_utc_shift 
+
+    # time array covering next 24 hours in steps of 5 min
+    elapsed = np.arange(0, (24*60)+5, 5)*u.min
+
+    #time array of next 24 hours starting from noon cest
+    time = time_in_local + elapsed 
+    time_array=time[:,None]
+
+    # Creating a series of coordinate frames in the AltAz system,
+    # each one corresponding to a specific time from the time array
+    # corresponding to a specific location given by obs
+
+    frame_local_24h = AltAz(obstime=time[:,None], location=_Loc)
+    Frame_Local_24h = AltAz(obstime=time, location=Loc)
+
+    #Betelgeuse RA and DEC   
+
+    Coordinates_of_the_stars=SkyCoord(ra=Data['ra'].values*u.deg, dec=Data['dec'].values*u.deg) 
+
+    # transforms the declination and right ascension of the
+    # star into altitudes and azimuths for the time sequence of frames defined
+
+    Coordinates_local = Coordinates_of_the_stars[None, :].transform_to(frame_local_24h)
+
+    # time-dependent coordinates of the Sun in equatorial system
+    sun = get_sun(time)
+    sun_local = sun.transform_to(Frame_Local_24h)
+
+    # night time w.r.t to the location
+    elapsed_night = elapsed[np.where(sun_local.alt < 0)]
+
+    if len(elapsed_night)==0:
+
+        raise ValueError('No night time detected for the given location at the given date')
+
+    #Coordinates of the stars when sun is down
+    Coord_local_sunset = Coordinates_local.alt[np.where(sun_local.alt < 0)]
+
+    #Observabilty Duration
+    Duration_of_Observabilty=[]
+
+    for i in range(len(Coord_local_sunset[0])):
+
+        if len(elapsed_night[Coord_local_sunset.transpose()[i]>0])>0:
+
+            Duration_of_Observabilty.append((elapsed_night[Coord_local_sunset.transpose()[i]>0][-1]-
+                                            elapsed_night[Coord_local_sunset.transpose()[i]>0][0]).to_value(u.min))
+        else:
+            Duration_of_Observabilty.append(0)
+
+    Data['Visibility (min)']=Duration_of_Observabilty
+
+    #Time of observability in Local timezone
+
+    Observability_Time_Local=[]
+    for i in range(len(Coord_local_sunset[0])):
+        
+        Observability_Time_Local.append(time_array[(Coordinates_local.alt.transpose()[i][:,None] > 0*u.deg)
+                                            & (sun_local.alt[:, None] < 0*u.deg)].to_datetime(timezone=timezone_local))
+
+    formatted_start=[]
+    formatted_end=[]
+
+    for dt in Observability_Time_Local:
+        if len(dt) > 0:
+            formatted_start.append(dt[0].strftime("%Y-%m-%d %H:%M"))
+            formatted_end.append(dt[-1].strftime("%Y-%m-%d %H:%M"))
+        else:
+            formatted_start.append("Not visible")
+            formatted_end.append("Not visible")
+
+    Data['Visibility (start)'] = formatted_start
+    Data['Visibility (end)'] = formatted_end
+
+    return Data
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as f:
+        encoded_string = base64.b64encode(f.read()).decode()
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{encoded_string}");
+            background-size: contain;
+            background-position: center;
+            background-attachment: fixed;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+# Adding bg image
+add_bg_from_local("milkyway.jpg")
+
+@st.cache_data
+def Coordinates(lat, long):
+    if is_numeric(lat) and is_numeric(long):
+
+        try:
+
+            tf = TimezoneFinder()
+            Loc = EarthLocation(lat=float(lat), lon=float(long))
+            tz_string = tf.timezone_at(lat=Loc.lat.deg, lng=Loc.lon.deg)
+            timezone_local = ZoneInfo(tz_string)
+            local_time = datetime.datetime.now(timezone_local)
+            utc_shift_p = local_time.utcoffset().total_seconds() / 3600
+            utc_shift = utc_shift_p * u.hour
+
+            return utc_shift, Loc, timezone_local, tz_string, local_time, utc_shift_p
+
+        except Exception as e:
+            raise ValueError('Please enter valid latitude and longitude')
+    else:
+        raise ValueError('Please enter valid latitude and longitude')
+
+# Create a container and center the inputs
+container = st.container()
+
+with container:
+    cols = st.columns([0.2,0.266,0.266,0.266])  # Create 4 equal columns
+    
+    with cols[1]: 
+        lat = st.text_input("Enter your latitude in signed DD format", key="lat")
+    
+    with cols[2]: 
+        long = st.text_input("Enter your longitude in signed DD format", key="long")
+
+    with cols[3]: 
+        date=st.date_input("Select observation date")
+
+    with cols[1]:
+
+        if lat and long:
+
+            try:
+
+                if ("prev_lat" not in st.session_state
+                    or st.session_state["prev_lat"] != lat
+                    or st.session_state["prev_long"] != long):
+
+                    utc_shift, Loc, timezone_local, tz_string, local_time, utc_shift_p = Coordinates(lat, long)
+                    
+                    st.session_state.update({
+                        "utc_shift": utc_shift,
+                        "Loc": Loc,
+                        "timezone_local": timezone_local,
+                        "prev_lat": lat,
+                        "prev_long": long,
+                        "tz_string": tz_string,
+                        "local_time": local_time,
+                        "utc_shift_p": utc_shift_p
+                    })
+
+                else:
+                    utc_shift = st.session_state["utc_shift"]
+                    Loc = st.session_state["Loc"]
+                    timezone_local = st.session_state["timezone_local"]
+                    tz_string = st.session_state["tz_string"]
+                    local_time = st.session_state["local_time"]
+                    utc_shift_p = st.session_state["utc_shift_p"]
+
+                # Streamlit outputs outside cached function
+                st.write("Time zone:", tz_string)
+                st.write("Local time:", local_time.strftime("%H:%M:%S"))
+                st.write("UTC Shift:", utc_shift_p, "h")
+
+                if tz_string.startswith("Etc/GMT"):
+                    st.warning('Location maybe a sea or desert', width=310)
+
+            except Exception as e:
+                st.error(f"{e}", width=309)
+
+# Container for the file uploader
+
+    Data=None
+
+    with cols[1]:  # Second column for file uploader
+        
+        uploaded_file = st.file_uploader("Upload your star catalog file", type=["csv", "txt", "dat"])
+
+        if uploaded_file is not None:
+            try:
+
+                Data, has_valid_magnitude = read_data(uploaded_file)
+                st.session_state["Data"] = Data
+                st.session_state["has_valid_magnitude"] = has_valid_magnitude
+                st.success("File Loaded Successfully", width=315)
+
+            except Exception as e:
+
+                st.error(f"{e}")
+
+    with cols[2]:
+
+        if uploaded_file is not None and lat and long and is_numeric(lat) and is_numeric(long):
+
+            st.markdown("<div style='margin-bottom: 165px;'></div>", unsafe_allow_html=True)
+            with st.expander("Data preview", expanded=False, width=360):
+
+                st.dataframe(Data, height=212)
+
+        elif uploaded_file is not None and lat and long:
+
+            st.markdown("<div style='margin-bottom: 115px;'></div>", unsafe_allow_html=True)
+            with st.expander("Data preview", expanded=False, width=360):
+
+                st.dataframe(Data, height=212)
+
+        elif uploaded_file is not None:
+
+            st.markdown("<div style='margin-bottom: 40px;'></div>", unsafe_allow_html=True)
+            with st.expander("Data preview", expanded=False, width=360):
+
+                st.dataframe(Data, height=212)
+
+preview_container=st.container()
+
+with preview_container:
+
+    pcols = st.columns([0.2,0.266,0.266,0.266]) 
+
+    if Data is not None and lat and long and is_numeric(lat) and is_numeric(long):
+
+        with pcols[1]:
+
+            dur=None
+            magn=None
+
+            checkbox=st.checkbox('Filters', key="filter_checkbox")
+
+            if checkbox: #Filter buttons
+                left,right=st.columns(2)
+
+                with left:
+                    dur=st.number_input('Duration >', value=None, key="dur")
+                
+                if has_valid_magnitude:
+                    with right:
+                        magn=st.number_input('magn >', value=None, key="mag")
+                
+                # Add consistent spacing after filters
+                st.markdown("<div style='margin-bottom: 10px;'></div>", unsafe_allow_html=True)
+            else:
+                # When filters are not checked, add the same amount of space
+                st.markdown("<div style='margin-bottom: 10px;'></div>", unsafe_allow_html=True)
+
+        with pcols[1]:
+                
+                if st.button('Calculate the time of observability'):
+                    
+                    try:
+                        
+                        Star_Observability = Observability(
+                            st.session_state["Data"],
+                            st.session_state["utc_shift"],
+                            date,
+                            st.session_state["Loc"],
+                            st.session_state["timezone_local"]
+                        )
+
+                        # store result for later preview
+                        st.session_state["Star_Observability"] = Star_Observability
+
+                        # Apply filters
+                        filtered_data = Star_Observability.copy()
+                        if 'Visibility (min)' in Star_Observability.columns and dur is not None:
+                            filtered_data = filtered_data[filtered_data['Visibility (min)'] > dur]
+
+                        if magn is not None:
+                            filtered_data = filtered_data[filtered_data['mag'] < magn]
+
+                        st.session_state["Filtered_Observability"] = filtered_data
+                        st.session_state["calculation_done"] = True
+
+                        with pcols[2]:
+
+                            if checkbox:
+                                st.markdown("<div style='margin-bottom: 148px;'></div>", unsafe_allow_html=True) 
+                                with st.expander("Observability preview", expanded=False, width=360):
+                                    st.dataframe(filtered_data, height=212)
+                            else:
+                                st.markdown("<div style='margin-bottom: 65px;'></div>", unsafe_allow_html=True) 
+                                with st.expander("Observability preview", expanded=False, width=360):
+                                    st.dataframe(Star_Observability, height=212)
+
+                    except Exception as e:
+                        st.error(f'{e}')
+
